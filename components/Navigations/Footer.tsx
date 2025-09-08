006a7a421e4822ccee5cd5322cf481b8a9f526d5
AlexLeiba <Alexleiba13@gmail.com>
2025-09-08 11:53:29 +0300

    contacts

--- components/Navigations/Footer.tsx
+++ components/Navigations/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GridContainer from "../Grid/GridContainer";
 import { Logo } from "./Logo";
+import { Github, Linkedin, Mail } from "lucide-react";
+import Link from "next/link";
 
 export function Footer() {
   return (
@@ -9,7 +11,31 @@ export function Footer() {
         <div className="flex justify-between items-center">
           <Logo />
 
-          <p className="text-lg">Contact</p>
+          <div className="flex gap-4">
+            <Link
+              href={"https://github.com/AlexLeiba"}
+              target="_blank"
+              title="Github"
+            >
+              <Github />
+            </Link>
+
+            <Link
+              href={"mailto:leiba.alexandru@gmail.com"}
+              target="_blank"
+              title="Gmail"
+            >
+              <Mail />
+            </Link>
+
+            <Link
+              href={"https://www.linkedin.com/in/alex-leiba-9205801ba/"}
+              target="_blank"
+              title="Linkedin"
+            >
+              <Linkedin />
+            </Link>
+          </div>
         </div>
       </GridContainer>
     </footer>
